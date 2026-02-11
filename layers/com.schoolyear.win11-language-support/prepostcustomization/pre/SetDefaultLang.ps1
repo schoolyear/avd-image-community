1549389c15debc7e5cc438455e2f901628d59472
UpwardsandOnwards <bovenjim@gmail.com>
2026-02-11 15:52:17 +0100

    try fix SSO

--- layers/com.schoolyear.win11-language-support/prepostcustomization/pre/SetDefaultLang.ps1
+++ layers/com.schoolyear.win11-language-support/prepostcustomization/pre/SetDefaultLang.ps1
@@ -208,6 +208,35 @@ $elapsedTime = $stopwatch.Elapsed
 Write-Host "*** AVD AIB CUSTOMIZER PHASE: Set default Language - Exit Code: $LASTEXITCODE ***"
 Write-Host "*** AVD AIB CUSTOMIZER PHASE: Set default Language - Time taken: $elapsedTime ***"
 
+$downloadUrl = "https://github.com/thebookisclosed/ViVe/releases/download/v0.3.4/ViVeTool-v0.3.4-IntelAmd.zip"  # URL to download ViVe tool
+$tempPath = "C:\Windows\Temp"
+$viveToolZip = "$tempPath\ViVeTool.zip"
+$viveToolDir = "$tempPath\ViVeTool"
+New-Item -Path $viveToolDir -ItemType Directory -Force | Out-Null
+
+$viveToolExe = "$viveToolDir\ViVeTool.exe"
+$featureIds = @(47557358, 45833058)
+
+
+# Ensure ViVeTool exists
+if (-not (Test-Path $viveToolExe)) {
+    Invoke-WebRequest -Uri $downloadUrl -OutFile "$tempPath\ViVeTool.zip"
+    Expand-Archive -Path "$tempPath\ViVeTool.zip" -DestinationPath $viveToolDir -Force
+    Write-host "Downloaded and extracted ViVeTool."
+} else {
+    Write-host "ViVeTool already exists."
+}
+# disable features
+foreach ($featureId in $featureIds) {
+    Write-host "Disabling feature with ID $featureId."
+& "$viveToolDir\ViveTool.exe" /disable /id:$featureId
+}
+ 
+# Query status of features
+foreach ($featureId in $featureIds) {  
+$queryresult = & "$viveToolDir\ViveTool.exe" /query /id:$featureId  
+Write-host $queryresult  
+}
 
 #############
 #    END    #
