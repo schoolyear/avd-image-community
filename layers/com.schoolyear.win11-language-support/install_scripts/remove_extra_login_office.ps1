8bc273dfac313547e962a9b719bd1457d57b1a31
UpwardsandOnwards <bovenjim@gmail.com>
2026-02-27 17:24:59 +0100

    Adds explanation

--- layers/com.schoolyear.win11-language-support/install_scripts/remove_extra_login_office.ps1
+++ layers/com.schoolyear.win11-language-support/install_scripts/remove_extra_login_office.ps1
@@ -1,3 +1,6 @@
+# This script optionally removes the extra sign-in for Office apps when changing to a European language.
+# The Digital Market Act interrupts SSO for Office, if the request is coming from a European device.
+# This script works around this interrupt.
 # As adapted from: https://call4cloud.nl/continue-to-sign-in-prompt-sso-dma/
 # and https://call4cloud.nl/fix-continue-to-sign-in-prompt-dma-sso-compliance/
 # tool used : https://github.com/thebookisclosed/ViVe/releases/download/v0.3.4/ViVeTool-v0.3.4-IntelAmd.zip
