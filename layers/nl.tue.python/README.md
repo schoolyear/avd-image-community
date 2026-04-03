9f6d2ece3d3a573dc8da2531d49d785ef11cc4b4
UpwardsandOnwards <bovenjim@gmail.com>
2026-04-03 12:10:52 +0200

    Removes example code and adds readme

---
+++ layers/nl.tue.python/README.md
@@ -0,0 +1,5 @@
+# Python and Visual Studio Code
+
+This layer installs Python, Visual Studio Code, and a small set of Python and Jupyter Notebook extensions for VS Code, including `ipykernel` and the VS Code Jupyter extension set.
+
+It also configures Python file associations, adds a Python icon, creates VS Code and File Explorer shortcuts for new users, and prepares `pip` to use the session host proxy during user login.
--- layers/nl.tue.python/install.ps1
+++ layers/nl.tue.python/install.ps1
@@ -1,18 +1,5 @@
-# <CAN BE REMOVED>
-# This script is executed during the preparation of the exam image
-# This script is executed before the sysprep step
-#
-# This script is executed in its own layer folder
-# So, any file in this image layer, is available in the current working directory
-#
-# Once all installation scripts are executed, all image layer files are deleted
-# If you want to persist a file in the image, you must copy it to another folder
-# </CAN BE REMOVED>
 
 Param (
-    # [Parameter(Mandatory = $true)]
-    # [string]$exampleParameter,                  # You can configure your own paramter in the properties.json5 file
-
     [Parameter(ValueFromRemainingArguments)]
     [string[]]$RemainingArgs                    # To make sure this script doesn't break when new parameters are added
 )
